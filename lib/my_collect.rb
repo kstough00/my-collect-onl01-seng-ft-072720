1950bc82f76f3e20eac242e3cd81ea1296f7a3bc
Kyle Stough <kstough00@gmail.com>
2020-08-03 19:53:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(languages)
   i = 0 
   new_collection = []
   while i < languages.length
-  new_collection << yield(array[i])
+  new_collection << yield(languages[i])
   i += 1 
   end
 new_colletion
