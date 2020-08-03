18c55ae35b47c216ffe730c3efdcbd6332133bdd
Kyle Stough <kstough00@gmail.com>
2020-08-03 19:54:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(languages)
+  i = 0 
+  new_collection = []
+  while i < languages.length
+  new_collection << yield(languages[i])
+  i += 1 
+  end
+new_collection
+end
 
+my_collect(list) {|i| i.split(" ").first}
